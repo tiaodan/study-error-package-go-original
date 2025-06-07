2597af81357080bf5841f67bcced0e3a8b3a2885
tiaodan <tiaodanbang@gmail.com>
2025-06-07 23:07:47 +0800

    0.0.0.1 封装可用 msg里可用带参数. 如%s %v errorPrintf()

--- errorutil/error.go
+++ errorutil/error.go
@@ -27,13 +27,34 @@ func ErrorPanic(err error, msg string) int {
 	err: 错误
 	msg: 错误信息
 返回值:
-	errorCode int 错误码 1 - 正常，0 - 异常
+	errorCode int 错误码 1 - 正常，0 - 异常  // 弃用
+	error: 错误
 */
-func ErrorPrint(err error, msg string) int {
-	errorCode := 1
+func ErrorPrint(err error, msg string) error {
+	// errorCode := 1 // 弃用
 	if err != nil {
-		errorCode = 0
-		log.Println(msg + ": " + err.Error())
+		// errorCode = 0 // 弃用
+		log.Println(msg + ": " + err.Error()) // 整合别的项目后，有自己写的log框架，改为log.Error()
 	}
-	return errorCode
+	// return errorCode // 弃用
+	return err
+}
+
+// 封装纯打印错误.可带参数的
+/*
+参数:
+	err: 错误
+	msg: 错误信息,可带参数如 %s ,%v
+返回值:
+	errorCode int 错误码 1 - 正常，0 - 异常 // 弃用
+	error: 错误
+*/
+func ErrorPrintf(err error, msg string, args ...any) error {
+	// errorCode := 1 // 弃用
+	if err != nil {
+		// errorCode = 0 // 弃用
+		log.Println(msg, args, " reason : ", err.Error()) // 整合别的项目后，有自己写的log框架，改为log.Error()
+	}
+	// return errorCode // 弃用
+	return err
 }
--- version.md
+++ version.md
@@ -1,2 +1,5 @@
 # 0.0.0.0 
 - 用go语言原生框架, 封装error, 避免频繁判断 if err != nil { return err }
+
+# 0.0.0.1
+- 封装可用 msg里可用带参数. 如%s %v errorPrintf()
